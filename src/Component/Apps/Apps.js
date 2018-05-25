61a4a35b8b56338ea125e9dea3b65f30892dade5
Maximilian Witte <maximilianwitte@outlook.de>
2018-05-25 23:47:17 +0200

    Initial Commit

---
+++ src/Component/Apps/Apps.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Apps extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <h2>Hallo.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Apps;
